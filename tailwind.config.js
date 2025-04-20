d265bacca87282f9d171078b87784caad1656199
Rakshita Baidya <119068178+Rakshita-Baidya@users.noreply.github.com>
2025-04-20 22:53:16 +0545

    major ui updates with sidebar collapsible

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
         primaryLogo: "#00B2B2",
         secondaryLogo: "#7C7C7C",
         darkText: "#1D2A2A",
-        darkerPrimary: "#018181",
+        darkerPrimary: "#008080",
         dangerRed: "#B20003",
         darkerDangerRed: "#660002",
       },
